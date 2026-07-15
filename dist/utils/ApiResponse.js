14d1d1bfc5198229233be05c5e7b1fadda8a646a
GauravMG <gauravmittal17396@gmail.com>
2026-07-15 14:07:55 +0530

    fix: frontend build

--- dist/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiResponse = void 0;
-const apiResponse = (message, data) => ({
-    success: true,
-    message,
-    data
-});
-exports.apiResponse = apiResponse;
--- dist/utils/id-card-layout.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPdfLayout = void 0;
-const constants_1 = require("../config/constants");
-const getPdfLayout = (size) => constants_1.PDF_LAYOUTS[size];
-exports.getPdfLayout = getPdfLayout;
